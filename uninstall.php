811589d78cd3093d6057d71a57c73e657d3fc268
Davod Saraei <davodsaraei@gmail.com>
2020-11-22 08:04:01 +0330

    fix bug

--- uninstall.php
+++ uninstall.php
@@ -2,7 +2,7 @@
 /**
  * Uninstall Affili Tracker
  *
- * @package     Takhfifan merchant's tracker
+ * @package     Affili merchant's tracker
  * @subpackage  Uninstall
  * @license     https://github.com/affili-ir/wordpress/blob/master/LICENSE
  * @since       1.1.0
@@ -13,16 +13,12 @@ if (!defined('WP_UNINSTALL_PLUGIN')) {
     exit;
 }
 
-
 global $wpdb;
+include_once __DIR__.'/classes/Installer.php';
 
-$wpdb->query("DROP TABLE IF EXISTS ".$wpdb->prefix.takhfifan\main::$table);
-
-global $wpdb;
-
-$table_name = $wpdb->prefix.classes\Installer::$table;
+$table_name = $wpdb->prefix.AffiliIR\Installer::$table;
 
 $sql = "DROP TABLE IF EXISTS {$table_name}";
 $wpdb->query($sql);
 
-delete_option(classes\Installer::$table.'_db_version');
+delete_option(AffiliIR\Installer::$table.'_db_version');
